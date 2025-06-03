675e1b5e02a44e3c741d083ad99651dfd7e32210
Pallavi Tarke <tarkeshubham2006@gmail.com>
2025-06-04 02:12:42 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "location"         { default = "East US" }
+variable "resource_group"   { default = "nodejs-rg" }
+variable "vm_name"          { default = "nodejs-vm" }
+variable "admin_username"   { default = "azureuser" }
